52eae6d30839b16f5c70b92088f44a6f2822b459
W. Brent Williams <wbrentwilliams@hotmail.com>
2018-12-20 17:26:05 -0500

    Add move_tile, check_win, shuffle_map refactor

--- src/main.rs
+++ src/main.rs
@@ -6,38 +6,30 @@ use std::env;
 fn main() {
     let (seed, iterations) = process_args(&env::args().collect());
 
-    let mut rng: rand::prelude::StdRng;
-    match seed {
-        Some(seed) => {
-            println!("seed: {}", seed);
-            rng = StdRng::seed_from_u64(seed as u64);
-        }
-        None => {
-            rng = StdRng::from_entropy();
-        }
-    }
-
-    let mut val: usize;
+    let iter: usize;
     match iterations {
         Some(iterations) => {
-            println!("iterations: {}", iterations);
-            val = iterations;
+            iter = iterations;
         }
         None => {
-            val = 10;
+            iter = 10;
         }
     }
 
     let mut map = make_map();
-    println!("Beginning map");
+    if check_win(&map) {
+        println!("Beginning map");
+    } else {
+        println!("Map shuffled");
+    }
     print_map(&map);
-    println!("Shuffle {} times.", val);
-    let mut ptr = find_empty(&mut map);
-    while val > 0 {
-        ptr = shuffle_map(&mut rng, &mut map, ptr);
-        val -= 1;
+    println!("Shuffle {} times.", iter);
+    shuffle_map(seed, iter, &mut map);
+    if check_win(&map) {
+        println!("Beginning map");
+    } else {
+        println!("Map shuffled");
         print_map(&map);
-        println!();
     }
 }
 
@@ -112,108 +104,92 @@ fn print_map(map: &Vec<Option<usize>>) {
     println!("{}", &out);
 }
 
-fn shuffle_map<R: Rng>(
-    rng: &mut R,
-    map: &mut Vec<Option<usize>>,
-    empty: Option<usize>,
-) -> Option<usize> {
-    match empty {
-        Some(empty) => {
-            return random_pick(rng, map, empty);
-        }
-        None => {
-            return find_empty(map);
-        }
-    }
-}
-
-fn find_empty(map: &Vec<Option<usize>>) -> Option<usize> {
+fn find_empty(map: &Vec<Option<usize>>) -> usize {
     let mut ctr = 0;
     for item in map {
         match item {
             None => {
-                return Some(ctr);
+                return ctr;
             }
             _ => (),
         }
         ctr += 1;
     }
-    return None;
+    print_map(map);
+    panic!("Map does not have an empty space!");
 }
 
-fn random_pick<R: Rng>(rng: &mut R, map: &mut Vec<Option<usize>>, pointer: usize) -> Option<usize> {
-    match pointer {
-        0 => {
-            return choose_two(rng, map, pointer, 1, 4);
+fn shuffle_map(seed: Option<usize>, iterations: usize, map: &mut Vec<Option<usize>>) {
+    let mut rng: rand::prelude::StdRng;
+    match seed {
+        Some(seed) => {
+            println!("seed: {}", seed);
+            rng = StdRng::seed_from_u64(seed as u64);
         }
-        1 => {
-            return choose_three(rng, map, pointer, 0, 2, 5);
+        None => {
+            rng = StdRng::from_entropy();
         }
-        2 => {
-            return choose_three(rng, map, pointer, 1, 3, 6);
-        }
-        3 => {
-            return choose_two(rng, map, pointer, 2, 7);
-        }
-        4 => {
-            return choose_three(rng, map, pointer, 0, 5, 8);
-        }
-        5 => {
-            return choose_four(rng, map, pointer, 1, 4, 6, 9);
-        }
-        6 => {
-            return choose_four(rng, map, pointer, 2, 5, 7, 10);
-        }
-        7 => {
-            return choose_three(rng, map, pointer, 3, 6, 11);
-        }
-        8 => {
-            return choose_three(rng, map, pointer, 4, 9, 12);
-        }
-        9 => {
-            return choose_four(rng, map, pointer, 5, 8, 10, 13);
-        }
-        10 => {
-            return choose_four(rng, map, pointer, 6, 9, 11, 14);
-        }
-        11 => {
-            return choose_three(rng, map, pointer, 7, 10, 15);
-        }
-        12 => {
-            return choose_two(rng, map, pointer, 8, 13);
-        }
-        13 => {
-            return choose_three(rng, map, pointer, 9, 12, 14);
-        }
-        14 => {
-            return choose_three(rng, map, pointer, 10, 13, 15);
-        }
-        15 => {
-            return choose_two(rng, map, pointer, 11, 14);
-        }
-        _ => (),
     }
-    return None;
+
+    let mut pointer = find_empty(map);
+    let mut iter = iterations;
+    while iter > 0 {
+        match pointer {
+            0 => {
+                pointer = choose_two(&mut rng, map, pointer + 1, pointer + 4);
+            }
+            1 | 2 => {
+                pointer = choose_three(&mut rng, map, pointer - 1, pointer + 1, pointer + 4);
+            }
+            3 => {
+                pointer = choose_two(&mut rng, map, pointer - 1, pointer + 4);
+            }
+            4 | 8 => {
+                pointer = choose_three(&mut rng, map, pointer - 4, pointer + 1, pointer + 4);
+            }
+            5 | 6 | 9 | 10 => {
+                pointer = choose_four(
+                    &mut rng,
+                    map,
+                    pointer - 4,
+                    pointer - 1,
+                    pointer + 1,
+                    pointer + 4,
+                );
+            }
+            7 | 11 => {
+                pointer = choose_three(&mut rng, map, pointer - 4, pointer - 1, pointer + 4);
+            }
+            12 => {
+                pointer = choose_two(&mut rng, map, pointer - 4, pointer + 1);
+            }
+            13 | 14 => {
+                pointer = choose_three(&mut rng, map, pointer - 4, pointer - 1, pointer + 1);
+            }
+            15 => {
+                pointer = choose_two(&mut rng, map, pointer - 4, pointer - 1);
+            }
+            _ => (),
+        }
+        iter -= 1;
+    }
 }
 
 fn choose_two<R: Rng>(
     rng: &mut R,
     map: &mut Vec<Option<usize>>,
-    pointer: usize,
     item_1: usize,
     item_2: usize,
-) -> Option<usize> {
+) -> usize {
     let val = rng.gen_range(0, 2);
     match val {
         1 => {
-            map[pointer] = map[item_1];
-            map[item_1] = None;
-            return Some(item_1);
+            move_tile(map, item_1);
+            return item_1;
         }
         0 => {
-            map[pointer] = map[item_2];
-            map[item_2] = None;
-            return Some(item_2);
+            move_tile(map, item_2);
+            return item_2;
         }
         _ => panic!("choose_two random number out of range: {}", val),
     }
@@ -222,27 +198,23 @@ fn choose_two<R: Rng>(
 fn choose_three<R: Rng>(
     rng: &mut R,
     map: &mut Vec<Option<usize>>,
-    pointer: usize,
     item_1: usize,
     item_2: usize,
     item_3: usize,
-) -> Option<usize> {
+) -> usize {
     let val = rng.gen_range(0, 3);
     match val {
         0 => {
-            map[pointer] = map[item_1];
-            map[item_1] = None;
-            return Some(item_1);
+            move_tile(map, item_1);
+            return item_1;
         }
         1 => {
-            map[pointer] = map[item_2];
-            map[item_2] = None;
-            return Some(item_2);
+            move_tile(map, item_2);
+            return item_2;
         }
         2 => {
-            map[pointer] = map[item_3];
-            map[item_3] = None;
-            return Some(item_3);
+            move_tile(map, item_3);
+            return item_3;
         }
         _ => panic!("choose_three random number out of range: {}", val),
     }
@@ -251,34 +223,80 @@ fn choose_three<R: Rng>(
 fn choose_four<R: Rng>(
     rng: &mut R,
     map: &mut Vec<Option<usize>>,
-    pointer: usize,
     item_1: usize,
     item_2: usize,
     item_3: usize,
     item_4: usize,
-) -> Option<usize> {
+) -> usize {
     let val = rng.gen_range(0, 4);
     match val {
         3 => {
-            map[pointer] = map[item_1];
-            map[item_1] = None;
-            return Some(item_1);
+            move_tile(map, item_1);
+            return item_1;
         }
         2 => {
-            map[pointer] = map[item_2];
-            map[item_2] = None;
-            return Some(item_2);
+            move_tile(map, item_2);
+            return item_2;
         }
         1 => {
-            map[pointer] = map[item_3];
-            map[item_3] = None;
-            return Some(item_3);
+            move_tile(map, item_3);
+            return item_3;
         }
         0 => {
-            map[pointer] = map[item_4];
-            map[item_4] = None;
-            return Some(item_4);
+            move_tile(map, item_4);
+            return item_4;
         }
         _ => panic!("choose_four random number out of range: {}", val),
     }
+}
+
+fn move_tile(map: &mut Vec<Option<usize>>, tile: usize) -> bool {
+    let empty = find_empty(&map);
+    let mv = empty as isize - tile as isize;
+    match mv {
+        -4 => {
+            map[empty] = map[tile];
+            map[tile] = None;
+            return true;
+        }
+        -1 => {
+            map[empty] = map[tile];
+            map[tile] = None;
+            return true;
+        }
+        1 => {
+            map[empty] = map[tile];
+            map[tile] = None;
+            return true;
+        }
+        4 => {
+            map[empty] = map[tile];
+            map[tile] = None;
+            return true;
+        }
+        _ => {
+            println!("Illegal move: {}", tile);
+            return false;
+        }
+    }
+}
+
+fn check_win(map: &Vec<Option<usize>>) -> bool {
+    let mut last_value: usize = 0;
+    for x in 0..15 {
+        let tmp = map[x];
+        match tmp {
+            Some(tmp) => {
+                if tmp == last_value + 1 {
+                    last_value = tmp;
+                } else {
+                    return false;
+                }
+            }
+            None => {
+                return false;
+            }
+        }
+    }
+    return true;
 }
